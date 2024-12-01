d32c6dc9c27f63365af0e5d0bd472d8c61fa7211
Elvin251 <emmmdov485@gmail.com>
2024-12-01 13:25:56 +0400

    d

---
+++ app.js
@@ -0,0 +1,33 @@
+//function add() {
+   // let num = document.getElementById("num");
+   // let Numb = +(num.innerText);
+   // num.innerText=Numb+1}
+   // function dadd() {
+      //  let num = document.getElementById("num");
+       // let Numb = +(num.innerText);
+        //num.innerText=Numb-1}
+        //function zero() {
+          //  let num = document.getElementById("num");
+          //  let Numb = +(num.innerText);
+           // num.innerText=0}
+
+
+        //    task1
+        function changeParagraphText() {
+     let paragraph = document.getElementById("myParagraph");
+     paragraph.innerText = "Salam Dunya"; 
+     
+        }
+        function hideParagraf() {
+            
+            document.getElementById("myParagraph").style.display="none";
+        }
+        function showParagraf() {
+            
+         document.getElementById("myParagraph").style.display="block";
+     }
+     function changeBackgroundColor() {
+            
+      document.getElementById("body").style.backgroundColor="blue";
+  }
+  
